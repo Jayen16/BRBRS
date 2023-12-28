5aa2a4cbfe0068c0b23a275c0aead62e1fcb0d8d
BRBRS <jnred0916@gmail.com>
2023-12-28 20:02:05 +0800

    Designed EmailVerification

--- database/migrations/2023_12_06_090216_create_categories_table.php
+++ database/migrations/2023_12_06_090216_create_categories_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -16,6 +17,23 @@ return new class extends Migration
             $table->string('category');
             $table->timestamps();
         });
+
+        $categories = [
+            'Fiction',
+            'Non-Fiction',
+            'Children\'s Book',
+            'Reference',
+            'Periodicals',
+            'Biography'
+        ];
+
+        foreach ($categories as $category) {
+            DB::table('categories')->insert([
+                'category' => $category,
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
     }
 
     /**
